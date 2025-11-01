395c550f48f64e4d774ed7b6013669b6be965852
Aleksandra <aaleksandra.simonova@gmail.com>
2025-11-01 14:34:05 +0300

    Add pipline module

--- src/pipelines/sdxl.py
+++ src/pipelines/sdxl.py
@@ -1,4 +1,5 @@
 import torch
+from diffusers import DiffusionPipeline
 
 class SDXLPipeline:
     def from_pretrained(model, *args, **kwargs):
@@ -6,5 +7,21 @@ class SDXLPipeline:
         # create pipeline
         # initizlize it with model submodules (like model.unet, model.text_encoder and etc.)
         # pipeline = ...
-        return pipeline
+
+        pipeline = DiffusionPipeline.from_pretrained(
+            model.pretrained_model_name_or_path,
+            unet=model.unet,
+            text_encoder=model.text_encoder,
+            text_encoder_2=model.text_encoder_2,
+            vae=model.vae,
+            tokenizer=model.tokenizer,
+            tokenizer_2=model.tokenizer_2,
+            scheduler=model.noise_scheduler,
+            torch_dtype=model.weight_dtype,
+            *args,
+            **kwargs
+        )
         
+        pipeline = pipeline.to(model.device)
+        
+        return pipeline 
